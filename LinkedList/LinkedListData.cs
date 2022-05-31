693d2f3362c87a819ea4c8878f0402a535e3ae91
[prasanna] <prasanna>
2022-05-31 11:37:38 +0530

    [prasanna]: adding elements in linked list

---
+++ LinkedList/LinkedListData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedListData
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void DisPlay()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+
+            }
+                
+            
+        }
+    }
+}
